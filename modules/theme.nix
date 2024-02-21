939ac7617d4277eba8127b649c72eebb0d5892ba
Christo Paul <christop23@proton.me>
2024-02-21 19:21:28 +0530

    update theme

--- modules/theme.nix
+++ modules/theme.nix
@@ -4,11 +4,11 @@
 {
     gtk.theme = {
         name = "Qogir";
-        package = pkgs.qogir-theme;
+        #package = pkgs.qogir-theme;
     };
     gtk.iconTheme = {
         name = "Qogir";
-        package = pkgs.qogir-icon-theme;
+        package = pkgs.Qogir-icon-theme;
     };
     qt = {
         enable = true;
